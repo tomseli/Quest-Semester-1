07cf0c697fdee49a63ffadb8e81130f2bfdc92ea
Jelmer <you@example.com>
2023-12-18 14:14:36 +0100

    fixed

--- uitest/uitest.py
+++ uitest/uitest.py
@@ -1,4 +1,5 @@
 from kivy.app import App
+from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.tabbedpanel import TabbedPanel, TabbedPanelHeader
 from kivy.uix.button import Button
@@ -12,6 +13,7 @@ from kivy.garden.graph import Graph, MeshLinePlot
 from kivy.properties import NumericProperty
 from kivy.clock import Clock
 import math
+
 
 
 
@@ -39,14 +41,19 @@ class MyApp(App):
     def build(self):
         Window.clearcolor = (0.15, 0.15, 0.15, 1)  # Dark background
 
+
+        #tabs
         tabbed_panel = TabbedPanel(do_default_tab=False, tab_pos='left_mid')
 
-        # Settings tab    
+        # Settings tab --------------------------------------------------------------------------------------------------------------------------	
         settings_tab = TabbedPanelHeader(text='Settings')
-        settings_layout = BoxLayout(orientation='vertical')
+        settings_layout = AnchorLayout()
 
-        # Add a placeholder graph at the top right
-        graph_layout = BoxLayout(orientation='horizontal', size_hint=(0.75, 1))
+
+        # Add V,t diagram
+        graph_layout = AnchorLayout(anchor_x='right', anchor_y='top')
+        graph_box = BoxLayout(size_hint=(None, None), size=(600, 400))  # Adjust size as needed
+
         placeholder_graph = Graph(xlabel='X', ylabel='Y', x_ticks_minor=5,
                                 x_ticks_major=25, y_ticks_major=1,
                                 y_grid_label=True, x_grid_label=True, padding=5,
@@ -54,52 +61,71 @@ class MyApp(App):
         plot = MeshLinePlot(color=[1, 0, 0, 1])
         plot.points = [(x, 0) for x in range(0, 101)]
         placeholder_graph.add_plot(plot)
-        graph_layout.add_widget(placeholder_graph)
-        settings_layout.add_widget(graph_layout)        
 
-        # Add slider 1
+        graph_box.add_widget(placeholder_graph)
+        graph_layout.add_widget(graph_box)
+        settings_layout.add_widget(graph_layout) 
+
+        # Add slider APM
+        slider_layout = AnchorLayout(anchor_x='left', anchor_y='bottom')
+        slider_box = BoxLayout(size_hint=(None, None), size=(700, 100))  # Adjust size as needed
+
         self.slider1 = Slider(min=0, max=100, step=1, value=50)
         self.slider1_label = Label(text=f'{self.slider1.value} ({self.slider1.min}-{self.slider1.max})')
         self.slider1.bind(value=self.update_slider1_label)
 
-        settings_layout.add_widget(self.slider1)
-        settings_layout.add_widget(self.slider1_label)
+        slider_box.add_widget(self.slider1)
+        slider_box.add_widget(self.slider1_label)
 
-        # Add switches
+        slider_layout.add_widget(slider_box)
+        settings_layout.add_widget(slider_layout)
+
+        # Add switches for presets
+        switch_layout = AnchorLayout(anchor_x='left', anchor_y='top')
+        switch_box = BoxLayout(orientation='vertical', size_hint=(None, None), size=(200, 200))  # Adjust size as needed
+
         self.switch1 = Switch(active=False)
         self.switch1_label = Label(text='Borst of buik')
         self.switch2 = Switch(active=False)
-        self.switch2_label = Label(text='Instructie en zonder')
+        self.switch2_label = Label(text='Instructie of zonder')
 
+        switch_box.add_widget(self.switch1_label)
+        switch_box.add_widget(self.switch1)
+        switch_box.add_widget(self.switch2_label)
+        switch_box.add_widget(self.switch2)
 
-        # layout switches
-        settings_layout.add_widget(self.switch1_label)
-        settings_layout.add_widget(self.switch1)
-        settings_layout.add_widget(self.switch2_label)
-        settings_layout.add_widget(self.switch2)
+        switch_layout.add_widget(switch_box)
+        settings_layout.add_widget(switch_layout)
 
-        # Start stop button
+        # Start-stop button
+        button_layout = AnchorLayout(anchor_x='right', anchor_y='bottom')
+        button_box = BoxLayout(size_hint=(None, None), size=(100, 100))  # Adjust size as needed
+
         start_stop_button = Button(text='Start or Stop')
         start_stop_button.bind(on_release=self.start_stop)
-        
-        #layout start stop button
-        settings_layout.add_widget(start_stop_button)
 
+        button_box.add_widget(start_stop_button)
+        button_layout.add_widget(button_box)
+        settings_layout.add_widget(button_layout)
+
+
+
+        #finish Settings tab
         settings_tab.content = settings_layout
         tabbed_panel.add_widget(settings_tab)        
 
-        # Graphs tab
+
+        #graph tab --------------------------------------------------------------------------------------------------------------------------
         graphs_tab = TabbedPanelHeader(text='Graphs')
         self.graph = MyGraph()
 
         # Create a BoxLayout to contain the slider and the graph
-        graph_layout = BoxLayout(orientation='horizontal')
+        graph_layout = AnchorLayout()
 
         # Add slider for adjusting the y scale of the graph
         self.y_scale_slider = Slider(min=0.1, max=2, value=1, orientation='vertical', size_hint_x = 0.1)
         self.y_scale_slider.bind(value=self.update_y_scale)
 
-        # Add the slider and the graph to the graph_layout
         graph_layout.add_widget(self.y_scale_slider)
         graph_layout.add_widget(self.graph)
 
@@ -107,8 +133,7 @@ class MyApp(App):
         self.x_scale_slider = Slider(min=0.1, max=2, value=1, size_hint_y = 0.1)
         self.x_scale_slider.bind(value=self.update_x_scale)
 
-        # Add the x scale slider below the graph_layout
-        main_layout = BoxLayout(orientation='vertical')
+        main_layout = AnchorLayout()
         main_layout.add_widget(graph_layout)
         main_layout.add_widget(self.x_scale_slider)
 
